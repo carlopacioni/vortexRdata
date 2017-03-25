f4263fd4e6002388cbda14707e59126adef96075
Florian Mayer <Florian.Mayer@dpaw.wa.govst login: Thu Sep 15 12:05:04 2016 from aws-guacamole-002.corporateict.domain.au>
2017-03-25 13:37:23 +0800

    follow CRAN reviewer suggestions: amend DESCRIRPTION, add examples, explain in cran-comments.md, bump version to 1.0.2

--- R/vortexRdata_pkg_data.R
+++ R/vortexRdata_pkg_data.R
@@ -54,10 +54,12 @@
 #' Campbell et al. (2016). Assessing the economic benefits of starling
 #'  detection and control to Western Australia. Australasian Journal of
 #'  Environmental Management, 23, 81-99.
+#'  \href{https://dx.doi.org/10.1080/14486563.2015.1028486}{DOI:10.1080/14486563.2015.1028486}
 #'
 #' Pacioni, C., Williams, M., Lacy RC, Spencer, P.B.S. and Wayne, A.F. (2017)
 #'  Predators and genetic fitness: key threatening factors for the conservation
 #'  of bettong species. Pacific Conservation Biology.
+#'  \href{https://doi.org/10.1071/PC17002}{DOI:10.1071/PC17002}
 #'
 #' @docType package
 #'
@@ -74,7 +76,9 @@ NULL
 #' @description A dataset with the results from the main Vortex scenarios used
 #'  in Campbell et al (2016). Vortex outputs, from the project named
 #'  "Starlingv3PopBased" (.dat files), were collated with \code{collate_dat}.
-#' @usage data(sta.main)
+#' @examples
+#' data(sta.main)
+#' head(sta.main)
 #' @format a \code{data.frame} with 1632 observations of 44 variables.
 #' @source Campbell et al. (2016). Assessing the economic benefits of starling
 #'  detection and control to Western Australia. Australasian Journal of
@@ -88,7 +92,9 @@ NULL
 #'  in every 5 year cycle. Vortex outputs, from the project named
 #'  "Starlingv3PopBased" and the sensitivity test scenario "MReductEvy5"
 #'  (.stdat files), were collated with \code{collate_dat}.
-#' @usage data(sta.evy5)
+#' @examples
+#' data(sta.evy5)
+#' head(sta.evy5)
 #' @format a \code{data.frame} with 1020 observations of 47 variables.
 #' @source Campbell et al. (2016). Assessing the economic benefits of starling
 #'  detection and control to Western Australia. Australasian Journal of
@@ -102,7 +108,9 @@ NULL
 #'  5 year cycle, maintaining 2011 levels of investment. Vortex outputs, from
 #'  the project named "Starlingv3PopBased" and the sensitivity test scenario
 #'  "MReduction_B11_09Evy5" (.stdat files), were collated with \code{collate_dat}.
-#' @usage data(sta.evy5.b11)
+#' @examples
+#' data(sta.evy5.b11)
+#' head(sta.evy5.b11)
 #' @format a \code{data.frame} with 1020 observations of 47 variables.
 #' @source Campbell et al. (2016). Assessing the economic benefits of starling
 #'  detection and control to Western Australia. Australasian Journal of
@@ -116,7 +124,9 @@ NULL
 #'   outputs, from the project named "Pacioni_et_al" and (Single-Factor)
 #'   sensitivity test scenario "ST_Classic" (.stdat files), were collated with
 #'   \code{collate_dat}.
-#' @usage data(pac.clas)
+#' @examples
+#' data(pac.clas)
+#' head(pac.clas)
 #' @format a \code{data.frame} of 2904 observations of 68 variables.
 #' @source Pacioni, C., Williams, M., Lacy RC, Spencer, P.B.S. and Wayne, A.F. (2017)
 #'   Predators and genetic fitness: key threatening factors for the conservation
@@ -129,7 +139,9 @@ NULL
 #'  analysis on demographic parameters. Vortex outputs, from the project named
 #'  "Pacioni_et_al" and (Latin Hypercube Sampling) sensitivity test scenario
 #'  "ST_LHS" (.stdat files), were collated with \code{collate_dat}.
-#' @usage data(pac.lhs)
+#' @examples
+#' data(pac.lhs)
+#' head(pac.lhs)
 #' @format A \code{data.frame} of 6171 observations of 68 variables.
 #' @source Pacioni, C., Williams, M., Lacy RC, Spencer, P.B.S. and Wayne, A.F. (2017)
 #'  Predators and genetic fitness: key threatening factors for the conservation
@@ -142,7 +154,9 @@ NULL
 #'  analysis on demographic parameters. Vortex outputs, from the project named
 #'  "Pacioni_et_al" and (Latin Hypercube Sampling) sensitivity test scenario
 #'  "ST_LHS" (.run files), were collated with \code{collate_run}.
-#' @usage data(pac.run.lhs)
+#' @examples
+#' data(pac.run.lhs)
+#' head(pac.run.lhs)
 #' @format A named list of two \code{data.frame}s:
 #'   run (153 obs, 7 var), lrun (153 obs, 8 var).
 #' @source Pacioni, C., Williams, M., Lacy RC, Spencer, P.B.S. and Wayne, A.F. (2017)
@@ -156,7 +170,9 @@ NULL
 #'  analysis on demographic parameters. Vortex outputs, from the project named
 #'  "Pacioni_et_al" and (Single-Factor) sensitivity test scenario "ST_Classic"
 #'  (.yr files), were collated with \code{collate_yr}.
-#' @usage data(pac.yr)
+#' @examples
+#' data(pac.yr)
+#' head(pac.yr)
 #' @format A named list of two \code{element}s:
 #'   all (8712 obs, 26 var), means (2904 obs, 25 var).
 #' @source Pacioni, C., Williams, M., Lacy RC, Spencer, P.B.S. and Wayne, A.F. (2017)
@@ -169,7 +185,9 @@ NULL
 #' @description Data from Pacioni et al. (2017) - sensitivity test scenario
 #' "ST_Classic" - were used to calculate the harmonic mean of adults and population
 #' sizes using \code{Nadults}.
-#' @usage data(pac.clas.Nadults)
+#' @examples
+#' data(pac.clas.Nadults)
+#' head(pac.clas.Nadults)
 #' @format A \code{data.frame} with 24 observations of 4 variables.
 #' @source Pacioni, C., Williams, M., Lacy RC, Spencer, P.B.S. and Wayne, A.F. (2017)
 #'  Predators and genetic fitness: key threatening factors for the conservation
@@ -181,7 +199,9 @@ NULL
 #' @description Data from Pacioni et al. (2017) - sensitivity test scenario
 #' "ST_Classic" - were used to calculate the effective population size
 #' sizes using \code{Ne}.
-#' @usage data(pac.clas.Ne)
+#' @examples
+#' data(pac.clas.Ne)
+#' head(pac.clas.Ne)
 #' @format A \code{data.frame} with 24 observations of 2 variables.
 #' @source Pacioni, C., Williams, M., Lacy RC, Spencer, P.B.S. and Wayne, A.F. (2017)
 #'  Predators and genetic fitness: key threatening factors for the conservation
@@ -193,7 +213,9 @@ NULL
 #' @description Data from Pacioni et al. (2017) - sensitivity test scenario
 #' "ST_Classic" - were used to generate a look-up table
 #' sizes using \code{lookup_table}.
-#' @usage data(pac.clas.lookup)
+#' @examples
+#' data(pac.clas.lookup)
+#' head(pac.clas.lookup)
 #' @format A \code{data.frame} with 24 observations of 8 variables.
 #' @source Pacioni, C., Williams, M., Lacy RC, Spencer, P.B.S. and Wayne, A.F. (2017)
 #'  Predators and genetic fitness: key threatening factors for the conservation
@@ -204,7 +226,9 @@ NULL
 #' @title Results of pairwise comparisons of simulation scenarios
 #' @description Results of pairwise comparisons of simulation scenarios included
 #' in the sensitivity test scenario "ST_Classic" using \code{pairwise}.
-#' @usage data(pac.clas.pairw)
+#' @examples
+#' data(pac.clas.pairw)
+#' head(pac.clas.pairw)
 #' @format A named list of 12 \code{element}s. See documentation for details.
 #' @source Pacioni, C., Williams, M., Lacy RC, Spencer, P.B.S. and Wayne, A.F. (2017)
 #'  Predators and genetic fitness: key threatening factors for the conservation
@@ -219,9 +243,9 @@ NULL
 #' @description A folder with Vortex outputs from Pacioni et al. (2017) used
 #'  to run examples and Vortex project file. NOTE: these simulations are shorter
 #'  than those presented in the paper (only 3 runs for 120 'Vortex-years').
-#' @usage
-#' # To retrieve the path to the files use:
-#' # system.file("extdata", "pacioni", package="vortexR")
+#' @examples
+#' pacioni_dir <- system.file("extdata", "pacioni", package="vortexR")
+#' cat("Pacioni example files:\n"); dir(pacioni_dir)
 #' @format One .xml file and several .run and .stdat files.
 #' @source Pacioni, C., Williams, M., Lacy RC, Spencer, P.B.S. and Wayne, A.F. (2017)
 #'  Predators and genetic fitness: key threatening factors for the conservation
@@ -231,9 +255,9 @@ NULL
 #' @name campbell
 #' @title Raw Vortex outputs from Campbell et al (2016)
 #' @description A folder with Vortex outputs from Campbell et al (2016).
-#' @usage
-#' # To retrieve the path to the files use:
-#' # system.file("extdata", "campbell", package="vortexR")
+#' @examples
+#' campbell_dir <- system.file("extdata", "campbell", package="vortexR")
+#' cat("Campbell example files:\n"); dir(campbell_dir)
 #' @format Several .dat and .stdat files.
 #' @source Campbell et al. (2016). Assessing the economic benefits of starling
 #'  detection and control to Western Australia. Australasian Journal of
--- cran-comments.md
+++ cran-comments.md
@@ -13,12 +13,12 @@ In all environments, the test results were:
 
 0 errors | 0 warnings | 1 note (+1 on winbuilder "oldrelease")
 
-* checking CRAN incoming feasibility ... NOTE
+* NOTE
     ```
-    Maintainer: 'Carlo Pacioni <C.Pacioni@Murdoch.edu.au>'
-    
+    Maintainer: ‘Carlo Pacioni <C.Pacioni@Murdoch.edu.au>’
+
     New submission
-    
+
     Possibly mis-spelled words in DESCRIPTION:
     
     Pacioni (12:9)
@@ -27,29 +27,28 @@ In all environments, the test results were:
     
     et (11:14, 12:17)
     
-    vortexR (3:35, 15:19)
-    
-    The Description field should not start with the package name,
-    'This package' or similar.
+    vortexR (3:35)
     ```
   
     We would like to confirm that this package is a new submission.
     
     We would like to confirm that flagged possible mis-spelling are false positives.
-    
-    We amended the Description to lead with "This package uses..." as per the CRAN 
-    reviewer's suggestion to do so.
 
 * checking package dependencies ... NOTE
   
     ```No repository set, so cyclic dependency check skipped```
   
     This NOTE only occurred on winbuilder, R version 3.2.5 "oldrelease".
-    vortexRdata has no dependencies, as it contains no R code besides the data 
-    (.rda and raw text files), therefore we see no risk of cyclic dependencies
-    limited to R version 3.2.5 but not occurring in all other tested environments.
+
+    This NOTE does not appear on local GNU/Linux and local Windows test environments, 
+    where we have control over the `.Rprofile`, or on the two other winbuilder
+    test environments.
     
-    This does not appear on GNU/Linux and local Windows test environments.
+    Based on [this answer](https://stat.ethz.ch/pipermail/r-package-devel/2015q3/000293.html)
+    by the winbuilder maintainer and [answers](http://stackoverflow.com/q/23164929/2813717) 
+    on Stackoverflow we suggest it could be a missing setting 
+    `options(repos = c(CRAN="http://cran.r-project.org"))`
+    in the `.Rprofile` on that test machine rather than a problem with this package.    
     
 ## Reviewer notes
 The package was submitted as version 1.0.0 to CRAN and was rejected with 
@@ -58,7 +57,21 @@ suggestions from one CRAN reviewer. The suggestions were addressed as follows:
 * The field `Description` in `DESCRIPTION` now leads with "This package uses..."
   as per the reviewer's suggestions, however this introduces a NOTE (see above).
 * The citations now include DOIs in APA style.
-* Version bumped to 1.0.1.
+* Version bumped to 1.0.1 and resubmitted to CRAN.
+
+A second reviewer requested the following amendments:
+
+* "Please omit the redundant 'The package' and rather start 'Contains ....'.
+    We have amended the Description, which also fixed the related NOTE.
+* "Please single quote 'vortexR'."
+    'vortexR' is now single-quoted in the `DESCRIPTION`'s field `Description`.
+* "Please add at least some simple examples."
+    All `@usage` examples were converted to `@examples`.
+    This will provide working examples of accessing every data object 
+    (e.g. `help("pac.clas.lookup")`) and every directory of raw data 
+    (e.g. `help("pacioni")`).
+    Furthermore, our main package `vortexR` provides advanced examples by 
+    parsing raw data provided by `vortexRdata` and analysing the parsed data.
 
 ## Downstream dependencies
 We have checked downstream dependencies of vortexR using devtools::revdep_check().
